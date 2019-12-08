89f139fc91ddc6f9050c3aaa6bd2b8c57480410b
Kathryn Lewis <lewiskm@bu.edu>
2019-12-08 16:20:09 -0500

    Create MainActivity3.java

---
+++ MainActivity3.java
@@ -0,0 +1,14 @@
+package com.example.catch_take_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity3);
+    }
+}
