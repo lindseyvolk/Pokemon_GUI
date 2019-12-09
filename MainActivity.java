abe79abd2221e949487e9e43c2f3c80a7174046c
Kevin Arseneault <kevinars@bu.edu>
2019-12-09 15:12:35 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,17 +2,27 @@ package com.example.pokemonselector2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     public void displayToast(String message)
     {
@@ -22,20 +32,33 @@ public class MainActivity extends AppCompatActivity {
     public void showBulbasaurClick(View view)
     {
         displayToast(getString(R.string.bulbasaur_select_message));
+        Button button = (Button)findViewById(R.id.button);
+        button.setEnabled(true);
+        button.setBackgroundColor(Color.GREEN);
+
     }
 
     public void showCharmanderClick(View view)
     {
         displayToast(getString(R.string.charmander_select_message));
+        Button button = (Button)findViewById(R.id.button);
+        button.setEnabled(true);
+        button.setBackgroundColor(Color.GREEN);
     }
 
     public void showPikachuClick(View view)
     {
         displayToast(getString(R.string.pikachu_select_message));
+        Button button = (Button)findViewById(R.id.button);
+        button.setEnabled(true);
+        button.setBackgroundColor(Color.GREEN);
     }
 
     public void showSquirtleClick(View view)
     {
         displayToast(getString(R.string.squirtle_select_message));
+        Button button = (Button)findViewById(R.id.button);
+        button.setEnabled(true);
+        button.setBackgroundColor(Color.GREEN);
     }
 }
