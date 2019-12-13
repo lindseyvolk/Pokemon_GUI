2e15362f2b450971d3ed34882c73936e972bfa77
ashhamlin <54999825+ashhamlin@users.noreply.github.com>
2019-12-12 21:21:10 -0500

    Add files via upload

---
+++ Player2WIns.java
@@ -0,0 +1,14 @@
+package com.example.winner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Player2WIns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player2_wins);
+    }
+}
